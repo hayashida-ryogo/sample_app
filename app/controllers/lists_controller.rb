5d44f87660401596b3fcc05585e53cc78033e18d
hayashida-ryogo <hayashidaryogo@gmail.com>
2023-05-13 04:56:32 +0000

    [Finish] app-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,15 +7,20 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
     @lists = List.all
   end
 
+  def show
+    @list = List.find(params[:id])
+  end
 
-
+  def edit
+    @list = List.find(params[:id])
+    
 
   private
 
